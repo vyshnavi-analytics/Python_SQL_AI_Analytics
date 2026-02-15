69ae8efeb748f2db85df826c20cc5cf461b515ab
Vyshnavi <vyshnavip2912@gmail.com>
2026-02-15 13:05:34 +0530

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,24 @@
+-- Total applications & hires by recruiter
+SELECT recruiter,
+       SUM(applications) AS total_applications,
+       SUM(hires) AS total_hires
+FROM recruitment_data
+GROUP BY recruiter;
 
+-- Conversion rate
+SELECT recruiter,
+       SUM(hires) * 1.0 / SUM(applications) AS conversion_rate
+FROM recruitment_data
+GROUP BY recruiter;
+
+-- Average time to hire
+SELECT recruiter,
+       AVG(avg_time_to_hire) AS avg_time_to_hire
+FROM recruitment_data
+GROUP BY recruiter;
+
+-- Cost per hire
+SELECT recruiter,
+       SUM(avg_cost) / SUM(hires) AS cost_per_hire
+FROM recruitment_data
+GROUP BY recruiter;
